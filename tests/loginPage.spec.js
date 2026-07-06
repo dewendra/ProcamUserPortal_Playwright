8501fbab9f75de80c8904330a6a57b56f418ad32
dewendrasingh <dewendra1.singh@gmail.com>
2026-07-06 14:47:43 +0530

    Initial Playwright project

---
+++ tests/loginPage.spec.js
@@ -0,0 +1,48 @@
+//@ts-check
+import { test } from "@playwright/test";
+
+//const LoginPage = require("pages/LoginPage");
+//const DiscountApplyPage = require("pages/DiscountApplyPage");
+//const PersonalDetailsPage = require("pages/PersonalDetailsPage");
+//const EventCriteriaPage = require("pages/EventCriteriaPage");
+//const CharityDetailsPage = require("pages/CharityDetailsPage");
+//const MerchandiseDetailsPage = require("pages/MerchandiseDetailsPage");
+//const OrderSummaryPage = require("pages/OrderSummaryPage");
+//const PaymentsOptionPage = require("pages/PaymentsOptionPage");
+//const TransactionSuccessPage = require("pages/TransactionSuccessPage");
+
+test("Procam Registration Login Page Test", async ({ page }) => {
+  // const loginPage = new LoginPage(page);
+  // await loginPage.goto();
+  // await loginPage.verifyLogo();
+  // await loginPage.verifyLogin();
+
+  // const discountApplyPage = new DiscountApplyPage(page);
+  // await discountApplyPage.registrationCode();
+
+  // const personalDetailsPage = new PersonalDetailsPage(page);
+  // await personalDetailsPage.enterPersonalDetails();
+
+  // const eventCriteriaPage = new EventCriteriaPage(page);
+  // await eventCriteriaPage.selectingEvent();
+  // //await page.pause();
+
+  // const charityDetailsPage = new CharityDetailsPage(page);
+  // await charityDetailsPage.EnterCharityDetails();
+
+  // await page.pause();
+
+  // const merchandiseDetailsPage = new MerchandiseDetailsPage(page);
+  // await merchandiseDetailsPage.EnterMerchandiseDetails();
+
+  // const orderSummaryPage = new OrderSummaryPage(page);
+  // await orderSummaryPage.OrderDetails();
+
+  // const paymentsOptionPage = new PaymentsOptionPage(page);
+  // await paymentsOptionPage.paymentdetails();
+
+  // const transactionSuccessPage = new TransactionSuccessPage(page);
+  // await transactionSuccessPage.transactionDetails();
+
+  await page.pause();
+});
