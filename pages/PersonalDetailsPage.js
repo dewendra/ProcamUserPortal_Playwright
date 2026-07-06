cb005268b3ce23fba73ab2f99db693a552cc6a16
dewendra <dewendra1.singh@gmail.com>
2026-07-06 17:09:21 +0530

    Added Personal Details Page

--- pages/PersonalDetailsPage.js
+++ pages/PersonalDetailsPage.js
@@ -11,6 +11,9 @@ class PersonalDetailsPage {
         this.dob = page.locator('//input[contains(@class,"calendar-input")]');
         this.month = page.locator('//select[contains(@class,"monthpicker")]');
         this.year = page.locator('//select[contains(@class,"yearpicker")]');
+        this.pincode = page.locator('//input[@id="search-text" and contains(@placeholder,"Enter pinocde")]');
+        this.address = page.locator('//input[@name="address"]');
+        this.nationality = page.locator('//app-select[@name="nationality"]//input');
     }
     async enterPersonalDetails() {
         await this.first_name.fill("Sanjay");
@@ -18,6 +21,10 @@ class PersonalDetailsPage {
         await this.last_name.fill("Saxena");
         await this.mobile.fill("9876543210");
         await this.gender_male.click();
+        await this.dob.click();
+        await this.pincode.fill("110024");
+        await this.address.fill("A-262, Defence Colony");
+        await this.nationality.fill("Indian");
     }
 }
 module.exports = PersonalDetailsPage;
